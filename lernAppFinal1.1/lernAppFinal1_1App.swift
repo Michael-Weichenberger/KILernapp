4ba38470e2b3ddad26c56335422519f7e569b377
Michael Weichenberger <Kasithegreat@icloud.com>
2025-09-04 12:21:47 +0200

    Initial Commit

---
+++ lernAppFinal1.1/lernAppFinal1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  lernAppFinal1_1App.swift
+//  lernAppFinal1.1
+//
+//  Created by Kasi  on 04.09.25.
+//
+
+import SwiftUI
+
+@main
+struct lernAppFinal1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
